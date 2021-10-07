4b4862998d5057ccc7a74b20b4a5b1070a4d9d37
devdabiri <mayowa.dabiri@launchbridge.ng>
2021-10-07 17:25:56 +0100

    prod env url updated

--- src/utils/addPaymentUrl.js
+++ src/utils/addPaymentUrl.js
@@ -1,6 +1,10 @@
 export const addPaymentUrl = (data) => {
   return {
     ...data,
-    paymenturl: `${process.env.REACT_APP_DEV_URL}/pay/${data.paymentSlug}`,
+    paymenturl: `${
+      process.env.REACT_APP_ENV === "dev"
+        ? process.env.REACT_APP_DEV_URL
+        : REACT_APP_PROD_URL
+    }/pay/${data.paymentSlug}`,
   };
 };
