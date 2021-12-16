e59036e650bd242cb521da45d26f2dfe2b7e1df2
devdabiri <mayowa.dabiri@launchbridge.ng>
2021-12-16 12:24:29 +0100

    bug fixes

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,7 +1,7 @@
 import { House } from "../icons";
 import Proceed from "../assets/proceed.svg";
 
-import { useLocation, Link, useParams } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { RightArrow } from "./../icons/index";
 import { useMemo } from "react";
 import { cryptos as cryptoList } from "../constants/index";
@@ -15,9 +15,11 @@ const Accounts = ({
   header,
   isBankAdded,
 }) => {
+  const { pathname } = useLocation();
   const { slug } = useParams();
 
   const cryptoName = useMemo(() => {
+    const sliced = search.substring(10);
     return cryptoList.find((item) => item.slug === slug);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
